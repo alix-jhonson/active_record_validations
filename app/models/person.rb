70db2a1db23389c34fe11570cf0725d64a1b3340
rana haseeb <alix@yahoo.com>
2022-06-24 14:26:05 +0500

    format_without_method_validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ActiveRecord::Base
-  validates :legacy_code, format: { with: /\A[a-zA-Z]+\z/,
+  validates :legacy_code, format: { without: /\A[a-zA-Z]+\z/,
     message: "only allows letters" }
 
 end
