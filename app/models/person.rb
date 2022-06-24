6ee7643f8b1d51173f43de43a754cd1f1acdef6f
rana haseeb <alix@yahoo.com>
2022-06-24 15:38:28 +0500

    error[]_method_validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,13 @@
 class Person < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 3 }
 end
-person = Person.new
-person.valid?
-person.errors.messages
 person = Person.new(name: "John Doe")
 person.valid?
-person.errors.messages
+person.errors[:name]
+person = Person.new(name: "JD")
+person.valid?
+person.errors[:name]
+person = Person.new
+person.valid?
+person.errors[:name]
+
