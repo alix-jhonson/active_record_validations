2127c7d15687cce42206b052a702c2f71bc03840
rana haseeb <alix@yahoo.com>
2022-06-24 16:21:33 +0500

    errors[:base]_method_validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,5 @@
 class Person < ActiveRecord::Base
-def a_method_used_for_validation_purposes
-    errors.add(:name, "cannot contain the characters !@#%*()_-+=")
-  end
-end
-person = Person.create(name: "!@#")
-person.errors[:name]
-person.errors.full_messages
-
+  def a_method_used_for_validation_purposes
+    errors[:base] << "This person is invalid because ..."
+     end
+       end
