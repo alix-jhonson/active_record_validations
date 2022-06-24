dfc7d989e551e327886af006496f260165ade6c0
rana haseeb <alix@yahoo.com>
2022-06-24 18:11:33 +0500

    strict_validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,5 @@
 class Person < ActiveRecord::Base
-  with_options if: :is_bio? do |bio|
-    admin.validates :password, length: { minimum: 10 }
-    admin.validates :email, presence: true
-
+ validates :name, presence: { strict: true }
 end
+Person.new.valid?
 
