ef37fe010f628bc5e4ef093ff85c48c581c62156
rana haseeb <alix@yahoo.com>
2022-06-24 14:33:39 +0500

    Presence_method_validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,3 @@
 class Person < ActiveRecord::Base
-  validates :name, length: { minimum: 2 }
-  validates :bio, length: { maximum: 500 }
-  validates :password, length: { in: 6..20 }
-  validates :registration_number, length: { is: 6 }
+  validates :name, :login, :email, presence: true
 end
