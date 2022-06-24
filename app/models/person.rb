6fbcfc9a9aa0f28a3e8b76e630f7d0dc0408bc0a
rana haseeb <alix@yahoo.com>
2022-06-24 14:30:32 +0500

    length_validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,6 @@
 class Person < ActiveRecord::Base
-  validates :legacy_code, format: { without: /\A[a-zA-Z]+\z/,
-    message: "only allows letters" }
-
+  validates :name, length: { minimum: 2 }
+  validates :bio, length: { maximum: 500 }
+  validates :password, length: { in: 6..20 }
+  validates :registration_number, length: { is: 6 }
 end
