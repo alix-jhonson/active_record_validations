cf6fc8eb0f3c9aff8680f45f0e3415d2933af59f
rana haseeb <alix@yahoo.com>
2022-06-24 17:44:11 +0500

    associations_method_validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,10 +1,4 @@
 class Person < ActiveRecord::Base
-  validates :name, presence: true, length: { minimum: 3 }
-    end
-person = Person.new
-person.valid?
-person.errors.size
-person = Person.new(name: "Andrea", email: "andrea@example.com")
-person.valid?
-person.errors.size
-
+  has_many :roles
+  validates_associated :roles
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220624063442) do
+ActiveRecord::Schema.define(version: 20220624123320) do
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20220624063442) do
     t.boolean  "login"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
